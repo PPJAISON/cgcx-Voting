a3fb89768a5c000e607101d380703dd7094569de
shelbincormsquare <shelbin@cormsquare.com>
2018-08-11 17:27:41 +0530

    mino

--- cgcx-Node/src/service/server03.js
+++ cgcx-Node/src/service/server03.js
@@ -16,6 +16,12 @@ const monk = require('monk');
 const db = monk(url);
 const app = express();
 
+app.use(function(req, res, next) { 
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+  next(); 
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
